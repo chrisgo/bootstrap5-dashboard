e0909c43128d4097bfff74ac149e3ae8c0f2ac62
Chris Go <chris@velocimedia.com>
2025-01-26 11:38:11 -0800

    using github raw works for jquery load to bypass CORS

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 // We are going to use the bootstrap 12-col grid
 // https://getbootstrap.com/docs/5.3/layout/grid/
 //
-// (1) Define the columns in terms of a 12-col grid (must add to 12) 
+// (1) Define the columns in terms of a 12-col grid (must add to 12)
 //
-// examples: 
+// examples:
 // - [8, 4]     // 2 columns with a wide left side and a thinner right side
-// - [4, 4, 4]  // 3 equal columns 
+// - [4, 4, 4]  // 3 equal columns
 //
 var columns = [4, 4, 4];
 
@@ -21,7 +21,7 @@ var columns = [4, 4, 4];
 //   column: 0,                     // column index based on the definition above
 //   y: 1,                          // default row position
 //   type: 'list',                  // or 'tile'
-//   icon: 'fa-regular fa-dot',     // any icon font that you have included 
+//   icon: 'fa-regular fa-dot',     // any icon font that you have included
 // }
 //
 var widgets = [
@@ -37,19 +37,19 @@ var widgets = [
 //     if (widgets[index].type == 'list') {
 //         height = HEIGHT_SM;
 //     }
-//     // (2.2) Add the rest of the properties into the widget 
+//     // (2.2) Add the rest of the properties into the widget
 //     widgets[index] = { ...obj, height, visible: true, color: '#007ac1', };
 // });
 //console.log(widgets);
-// (3) Push them a layout object 
+// (3) Push them a layout object
 // var layout = {
 //     metadata: null,
-//     widgets, 
+//     widgets,
 //     theme: 'light',
 //     gridSize: { columns: 12, rows: null, },
 // }
 
-// initialize the dashboard 
+// initialize the dashboard
 var d = dashboard.initialize({
     // container: '#div-dashboard',
     // widgets,
